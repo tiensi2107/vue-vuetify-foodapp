658ac868d3d50727dd84acb7ea0714b775dc8faa
tiensi2107 <tiensi2107@gmail.com>
2021-12-24 16:59:15 +0700

    init

---
+++ src/components/SideBarLeft.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-navigation-drawer app color="green accent-3" dark>
+    <v-list-item class="px-2 py-5">
+      <v-list-item-title class="text-capitalize" align="center">
+        <v-icon x-large class="pr-2">fas fa-hamburger</v-icon>
+        <span class="text-h5">Food APP</span>
+      </v-list-item-title>
+    </v-list-item>
+    <v-list shaped>
+      <v-list-item-group v-model="selectedItem" color="grey lighten-5">
+        <v-list-item v-for="(item, index) in items" :key="index">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedItem: 0,
+      items: [
+        { icon: "fas fa-shipping-fast", text: "OVERVIEW", route: "/overview" },
+        { icon: "fas fa-bars", text: "MENU", route: "/menu" },
+        {
+          icon: "fas fa-dollar-sign",
+          text: "TRANSACTION",
+          route: "/transaction",
+        },
+        { icon: "fas fa-calendar-alt", text: "CALENDAR", route: "/calendar" },
+        {
+          icon: "fas fa-cogs",
+          text: "TESTIMONTIALS",
+          route: "/testimontials",
+        },
+        { icon: "fas fa-question", text: "FAQ", route: "/faq" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
